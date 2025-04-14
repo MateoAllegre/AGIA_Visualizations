1e17d1435056e9c08cfdc1be7227ce3342ee5d55
MateoAllegre <mateo.allegre@etu.uca.fr>
2025-04-14 16:45:30 +0100

    Code cleanup, allowing to easily add new meshes and test performance

--- babylonScene.js
+++ babylonScene.js
@@ -1,61 +1,120 @@
+// Reference files, in order for intellisense to work with babylon functions and types
 /// <reference path="babylon.d.ts" />
 /// <reference path="babylon.gui.d.ts" />
 
+"use strict"; // strict mode to guarantee better coding
+
+// HTML elements (divs) for the render canvas, and for the fps counter
 var canvas = document.getElementById("renderCanvas");
 let divFps = document.getElementById("fps");
-var createScene = function () {
-    var scene = new BABYLON.Scene(engine);
-	var camera = new BABYLON.ArcRotateCamera("camera",
-											BABYLON.Tools.ToRadians(90),
-											BABYLON.Tools.ToRadians(90),
-											12, BABYLON.Vector3.Zero(),
-											scene);
-		//camera.wheelDeltaPercentage = 0.02;
-		camera.wheelPrecision = 50;
-		camera.attachControl(canvas, true);
-    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-        
-	// BABYLON.ImportMeshAsync("https://babylontest.netlify.app/fountain.glb", scene).then(function (result) {
-    //     //result.meshes[0].scaling = new BABYLON.Vector3(2,2,2);
-	// 	//result.meshes[0].position = new BABYLON.Vector3(0.5,-1.5,2);
-    // });
 
-	// UI Base
-	var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+// Global variables
+var meshes = []; // List of currently loaded meshes
+var menuButton; // Menu button, needs to be global so that all buttons can enable it on click.
+var buttonPanel; // Panel containing mesh buttons, global so that mesh buttons can add themselves to it
+var activeMeshIndex; // Index (in meshes array) of most recently rendered mesh
 
-	var fountainMesh = null;
+// This function takes a name and a file, and creates a button in the menu with "name" as text
+// that loads and displays the mesh pointed by "meshSource", which can be a filepath or URL.
+const createMeshButton = function (name, meshSource, meshOperations) {
+	// Allocates a space in the array to store the mesh in, once it's loaded (after button click)
+	let index = meshes.length;
+	meshes.push(null);
 
-	// Fountain button
-	var fountainButton = BABYLON.GUI.Button.CreateSimpleButton("loadFountain", "Fontaine");
-	fountainButton.width = "150px";
-	fountainButton.height = "50px";
-	fountainButton.background = "red";
-	fountainButton.color = "darkred";
-	fountainButton.cornerRadius = 3;
-	fountainButton.thickness = 3;
-	fountainButton.onPointerUpObservable.add(() => {
-		fountainButton.isVisible = false;
-		if(fountainMesh != null) {
-			fountainMesh.enabled = true;
+	// Mesh button configuration
+	var button = BABYLON.GUI.Button.CreateSimpleButton("load" + name, name);
+	button.width = "250px";
+	button.height = "100px";
+	button.fontSize = "23px";
+	button.background = "red";
+	button.color = "darkred"; // text and border color
+	button.cornerRadius = 3; // rounds the corner of the rectangle by 3px
+	button.thickness = 3; // thickness of the border
+	button.paddingBottom = "5px"; // adds spacing between buttons in the panel
+
+	// On Click behaviour
+	button.onPointerUpObservable.add(() => {
+		// hides the menu buttons and sets activeMeshIndex
+		buttonPanel.isVisible = false;
+		activeMeshIndex = index;
+
+		if(meshes[index] != null) {
+			// If mesh is already loaded, reenable it and show "Back to Menu" button
+			meshes[index].setEnabled(true);
 			menuButton.isVisible = true;
 		} else {
-			console.time("Chargement du modele");
-			console.log("Debut du chargement du modele");
+			// If mesh isn't loaded, load it.
+			// Load time is measured by console.time() and timeEnd()
+			// The BabylonJS loading UI is shown during load (it can and will be configured)
+			
+			// Start timing and show load screen
+			console.time("Loading " + name);
+			console.log("Start loading of " + name);
 			engine.displayLoadingUI();
-			BABYLON.ImportMeshAsync("./cube.glb", scene).then(function (result) {
-				//result.meshes[0].scaling = new BABYLON.Vector3(2,2,2);
-				//result.meshes[0].position = new BABYLON.Vector3(0.5,-1.5,2);
-				fountainMesh = result.meshes[0];
-				console.timeEnd("Chargement du modele");
-				engine.hideLoadingUI();
-				menuButton.isVisible = true;
+			
+			// Load the actual mesh asynchronously (returns a Promise)
+			BABYLON.ImportMeshAsync(meshSource, scene).then(function (result) {
+				// After load succeeds, put the mesh reference in the correct slot of the array
+				meshes[index] = result.meshes[0];
+				if(typeof meshOperations !== "undefined") meshOperations(meshes[index]); // Apply given operations to mesh, if any
+				console.timeEnd("Loading " + name); // Stop timing (that also logs the time)
+				engine.hideLoadingUI(); // hide the load screen
+				menuButton.isVisible = true; // show menu button (after load only, or you can click it through the load screen which breaks everything)
 			});
 		}
 	});
-	advancedTexture.addControl(fountainButton);
 
-	// Menu button
-	var menuButton = BABYLON.GUI.Button.CreateSimpleButton("menu", "Retour au Menu");
+	// Add the button to UI, return it so that its properties can be changed if needed
+	buttonPanel.addControl(button);
+	return button;
+}
+
+var createScene = function () {
+	// Creation of the scene
+    var scene = new BABYLON.Scene(engine);
+
+	// Arc Rotate Camera, which can be panned, zoomed and rotated
+	var camera = new BABYLON.ArcRotateCamera("camera",
+											BABYLON.Tools.ToRadians(90), // starts at longitudinal angle 90
+											BABYLON.Tools.ToRadians(90), // starts at latitudinal angle 90
+											12, // starts at a distance of 12 units
+											BABYLON.Vector3.Zero(), // initial pivot is the origin
+											scene);
+		//camera.wheelDeltaPercentage = 0.02; // slows down zooming. Has issues. wheelPrecision is better.
+		camera.wheelPrecision = 50; // slows down the zooming (mouse wheel) by a factor of 50
+		camera.attachControl(canvas, true);
+
+	// Basic light source, shining down
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+        
+	// Import mesh from an online source
+	// Target website must implement CORS so that it can serve a file to another domain
+	// BABYLON.ImportMeshAsync("https://babylontest.netlify.app/fountain.glb", scene).then(function (result) {
+    //     // Resize and move mesh after import
+		   //result.meshes[0].scaling = new BABYLON.Vector3(2,2,2);
+	//     //result.meshes[0].position = new BABYLON.Vector3(0.5,-1.5,2);
+    // });
+
+	// UI Base (all UI elements are children of this)
+	var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+	// Creation of a Stack Panel to store the buttons for each mesh
+	buttonPanel = new BABYLON.GUI.StackPanel();
+	advancedTexture.addControl(buttonPanel); // add to UI
+
+	// Creation of the buttons for each fountain mesh
+	createMeshButton("Fountain", "./fountain.glb");
+	createMeshButton("Fountain Light", "./fountainLight.glb");
+	createMeshButton("Fountain Downscaled", "./fountainDownscaled.glb");
+	createMeshButton("Fountain Minimal", "./fountainLightDownscaled.glb");
+
+	// For Monstree, move the mesh after load, because it is not centered
+	createMeshButton("Monstree", "./monstree.glb", (mesh) => {
+		mesh.position = new BABYLON.Vector3(0.5,-1.5,2);
+	});
+
+	// Menu button configuration
+	menuButton = BABYLON.GUI.Button.CreateSimpleButton("menu", "Back to Menu");
 	menuButton.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
 	menuButton.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
 	menuButton.left = "-20px";
@@ -66,39 +125,46 @@ var createScene = function () {
 	menuButton.color = "darkred";
 	menuButton.cornerRadius = 3;
 	menuButton.thickness = 3;
+
+	// On Click behaviour for the menu button
 	menuButton.onPointerUpObservable.add(() => {
-		fountainButton.isVisible = true;
+		// Make menu visible and hide menu button
+		buttonPanel.isVisible = true;
 		menuButton.isVisible = false;
-		if(fountainMesh != null) fountainMesh.enabled = false;
+
+		// Disable current mesh (makes it invisible and improves performance)
+		meshes[activeMeshIndex].setEnabled(false);
 	});
+
+	// Add menu button to the UI and make it invisible
 	advancedTexture.addControl(menuButton);
 	menuButton.isVisible = false;
 
-	// console.time("Chargement du modele");
-	// console.log("Debut du chargement du modele");
-	// BABYLON.ImportMeshAsync("./fountain.glb", scene).then(function (result) {
-    //     //result.meshes[0].scaling = new BABYLON.Vector3(2,2,2);
-	// 	//result.meshes[0].position = new BABYLON.Vector3(0.5,-1.5,2);
-	// 	console.timeEnd("Chargement du modele");
-    // });
+	// Allows access to the debug mode of BabylonJS, including an inspector.
+	// Convenient for debugging.
+	// scene.debugLayer.show();
 
-	//scene.debugLayer.show();
-	//scene.performancePriority = BABYLON.ScenePerformancePriority.Intermediate;
+	// Change of performance settings in Babylon. May or may not have an impact on FPS.
+	// Stops automatically clearing the canvas after each draw, ruining the visual aspect
+	// scene.performancePriority = BABYLON.ScenePerformancePriority.Intermediate;
 	
     return scene;
 };
 
+// Creation of the Babylon Engine and the scene
 var engine = new BABYLON.Engine(canvas, true, { preserveDrawingBuffer: true, stencil: true });
 var scene = createScene();
 
-   engine.runRenderLoop(function () {
+// Definition of the render loop
+engine.runRenderLoop(function () {
+	// If the scene has been created
 	if (scene) {
-		scene.render();
-		divFps.innerHTML = engine.getFps().toFixed() + " FPS"
+		scene.render(); // Render the frame in the scene
+		divFps.innerHTML = engine.getFps().toFixed() + " FPS"; // Update the FPS counter
 	}
 });
 
-// Resize
+// Resize when necessary
 window.addEventListener("resize", function () {
 	engine.resize();
 });
